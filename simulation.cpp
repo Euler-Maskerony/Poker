f34af5dcf35663f61da6a4b8ba7d60aec3b62139
Euler-Maskerony <62300405+Euler-Maskerony@users.noreply.github.com>
2021-02-28 13:46:22 +0300

    Simulation has been completed

--- simulation.cpp
+++ simulation.cpp
@@ -1,68 +1,13 @@
-#include <set>
-#include <iostream>
 #include <algorithm>
-#include <vector>
 #include <utility>
+#include <ctime>
+#include "simulation.h"
 
-enum CardValue
+Card::Card()
 {
-    two,
-    three,
-    four,
-    five,
-    six,
-    seven,
-    eight,
-    nine,
-    ten,
-    jack,
-    queen,
-    king,
-    ace
-};
-
-enum CardSuit
-{
-    club,
-    diamond,
-    heart,
-    spade
-};
-
-enum Combinations
-{
-    high_card,
-    pair,
-    two_pairs,
-    triple,
-    straight,
-    flash,
-    fullhouse,
-    square,
-    straight_flash,
-    flash_royale
-};
-
-class Card
-{
-public:
-    int value;
-    int suit;
-
-    Card(int value, int suit) : value(value), suit(suit) {};
-
-    Card()
-    {
-        this->value = std::rand() % (ace+1);
-        this->suit = std::rand() % (spade+1);
-    }
-
-    friend bool operator>(const Card& a, const Card& b);
-    friend bool operator<(const Card& a, const Card& b);
-    friend bool operator==(const Card& a, const Card& b);
-    friend bool operator==(const Card& a, const int b);
-    friend int operator-(const Card& a, const Card& b);
-};
+    this->value = std::rand() % (ace-two+1) + two;
+    this->suit = std::rand() % (spade+1);
+}
 
 bool operator>(const Card& a, const Card& b)
 {
@@ -89,102 +34,88 @@ int operator-(const Card& a, const Card& b)
         return a.value - b.value;
 }
 
-typedef std::pair<Card,Card> Hand;
-
-class Combination
+Combination::Combination(std::vector<Card> cards)
 {
-private:
-    bool combs[10];  // just for convinience of checking
-
-    void cut(int begin, int len, int pos, std::string& str)
+    int f,l;
+    for(int i(0); i < 10; ++i)
+        combs[i] = false;
+    sort(cards.begin(),cards.end(),std::greater<Card>());
+    combs[flash] = true;
+    combs[high_card] = true;
+    for(int i(0); i < 5; ++i)
+        info += cards[i].value;
+    sort(info.begin(), info.end(), std::greater<char>());
+    for(int i(0); i < 5; ++i)
     {
-        str.replace(pos,len,str,begin,len);
+        if(i and combs[flash])
+            if(cards[i-1].suit != cards[i].suit)
+                combs[flash] = false;
+    }
+    for(int i(0); i < 5; ++i)
+    {
+        f = std::distance(cards.begin(), std::find(cards.begin(), cards.end(), cards[i]));
+        std::reverse(cards.begin(), cards.end());
+        l = std::distance(std::find(cards.begin(), cards.end(), cards[4-i]), cards.end())-1;
+        std::reverse(cards.begin(), cards.end());
+        switch(l - f + 1)
+        {
+        case 2:
+            if(combs[pair])
+            {
+                combs[two_pairs] = true;
+                cut(i,2,2,info);
+            }
+            else if(combs[triple])
+                combs[fullhouse] = true;
+            else if(not combs[two_pairs])
+            {
+                combs[pair] = true;
+                cut(i,2,0,info);
+            }
+            break;
+        case 3:
+            if(combs[pair])
+            {
+                combs[fullhouse] = true;
+                cut(i,3,0,info);
+            }
+            else
+            {
+                combs[triple] = true;
+                cut(i,3,0,info);
+            }
+            break;
+        case 4:
+            combs[square] = true;
+            cut(i,4,0,info);
+            break;
+        default:
+            break;
+        }
+        i = l;
+    }
+    if(not combs[pair] and not combs[triple] and not combs[square] and cards[0] - cards[4] == 4)
+        combs[straight] = true;
+    if(combs[straight] and combs[flash])
+        combs[straight_flash] = true;
+    if(combs[straight_flash] and cards[0] == ace)
+        combs[flash_royale] = true;
+    
+    for(int i(flash_royale); i >= high_card; --i)
+        if(combs[i])
+        {
+            c_type = i;
+            break;
+        }
+    if(c_type == flash)
+        sort(info.begin(), info.end(), std::greater<char>());
+}
+
+void Combination::cut(int begin, int len, int pos, std::string& str)
+    {
+        str.insert(pos,str.substr(begin,len));
         str.erase(begin+len,len);
     }
-
-public:
-    int c_type;
-    std::string info{""};
-
-    Combination() = delete;
-
-    Combination(std::vector<Card> cards)
-    {
-        int f,l;
-        for(int i(0); i < 10; ++i)
-            combs[i] = false;
-        sort(cards.begin(),cards.end(),std::greater<Card>());
-        combs[flash] = true;
-        combs[high_card] = true;
-        for(int i(0); i < 5; ++i)
-            info += std::to_string(cards[i].value);
-        sort(info.begin(), info.end(), std::greater<char>());
-        for(int i(0); i < 5; ++i)
-        {
-            if(i and combs[flash])
-                if(cards[i-1].suit != cards[i].suit)
-                    combs[flash] = false;
-            f = std::distance(cards.begin(), std::find(cards.begin(), cards.end(), cards[i]));
-            std::reverse(cards.begin(), cards.end());
-            l = std::distance(std::find(cards.begin(), cards.end(), cards[4-i]), cards.end())-1;
-            std::reverse(cards.begin(), cards.end());
-            switch(l - f + 1)
-            {
-            case 2:
-                if(combs[pair])
-                {
-                    combs[two_pairs] = true;
-                    cut(i,2,2,info);
-                }
-                else if(combs[triple])
-                    combs[fullhouse] = true;
-                else if(not combs[two_pairs])
-                {
-                    combs[pair] = true;
-                    cut(i,2,0,info);
-                }
-                break;
-            case 3:
-                if(combs[pair])
-                {
-                    combs[fullhouse] = true;
-                    cut(i,3,0,info);
-                }
-                else
-                {
-                    combs[triple] = true;
-                    cut(i,3,0,info);
-                }
-                break;
-            case 4:
-                combs[square] = true;
-                cut(i,4,0,info);
-                break;
-            default:
-                break;
-            }
-            i = l;
-        }
-        if(not combs[pair] and not combs[triple] and not combs[square] and cards[0] - cards[4] == 4)
-            combs[straight] = true;
-        if(combs[straight] and combs[flash])
-            combs[straight_flash] = true;
-        if(combs[straight_flash] and cards[0] == ace)
-            combs[flash_royale] = true;
-        
-        for(int i(flash_royale); i >= high_card; --i)
-            if(combs[i])
-            {
-                c_type = i;
-                break;
-            }
-        if(c_type == flash)
-            sort(info.begin(), info.end(), std::greater<char>());
-    }
-
-    friend bool operator<(const Combination& a, const Combination& b);
-    friend bool operator>(const Combination& a, const Combination& b);
-};
 
 bool operator<(const Combination& a, const Combination& b)
 {
@@ -198,12 +129,34 @@ bool operator>(const Combination& a, const Combination& b)
     else { return a.info > b.info; }
 }
 
-class Player
+bool operator==(const Combination& a, const Combination& b)
 {
-private:
-    std::vector<Card> all_cards{7};
+    if(a.c_type != b.c_type) { return a.c_type == b.c_type; }
+    else { return a.info == b.info; }
+}
 
-    bool next_combination (int mask[5]) 
+void Player::process(std::vector<Card> board)
+{
+    all_cards[0] = hand.first;
+    all_cards[1] = hand.second;
+    int n(4);
+    int mask[n+1];
+    for(int i(2); i < 7; ++i)
+    {
+        all_cards[i] = board[i-2];
+        mask[i-2] = i-2;
+    }
+    int i;
+    std::vector<Card> comb{5};
+    while(next_combination(mask))
+    {
+        for(int i(0); i < 5; ++i)
+            comb[i] = all_cards[mask[i]];
+        combinations.insert(Combination(comb));
+    }
+}
+
+bool Player::next_combination (int mask[5]) 
     {
 	    for (int i=4; i>=0; --i)
 		    if (mask[i] < 2+i) {
@@ -214,40 +167,6 @@ private:
 		}
 	    return false;
     }
-
-public:
-    std::set<Combination,std::greater<Combination>> combinations;
-    long long stack{-1};
-    Hand hand;
-    bool host{false};
-
-    Player(Hand hand) : hand(hand) {}
-    Player(Hand hand, long long stack) : hand(hand), stack(stack) {}
-
-    void process(std::vector<Card> board)
-    {
-        all_cards[0] = hand.first;
-        all_cards[1] = hand.second;
-        int n(4);
-        int mask[n+1];
-        for(int i(2); i < 7; ++i)
-        {
-            all_cards[i] = board[i-2];
-            mask[i-2] = i-2;
-        }
-        int i;
-        std::vector<Card> comb{5};
-        while(next_combination(mask))
-        {
-            for(int i(0); i < 5; ++i)
-                comb[i] = all_cards[mask[i]];
-            combinations.insert(Combination(comb));
-        }
-    }
-
-    friend bool operator>(const Player& a, const Player& b);
-    friend bool operator<(const Player& a, const Player& b);
-};
 
 bool operator>(const Player& a, const Player& b)
 {
@@ -273,43 +192,57 @@ bool operator<(const Player& a, const Player& b)
     }
 }
 
-class Game
+Game::Game(Hand host_hand, std::vector<Card> board, int players_num)
 {
-private:
-    std::vector<Player> players;
-    std::vector<Card> board{5};
-    int last;
-    bool win;
-public:
-    Game() = delete;
-
-    Game(Hand host_hand, std::vector<Card> board, int players_num)
+    std::srand(std::time(NULL));
+    for(Card el : board)
     {
-        for(Card el : board)
-        {
-            this->board[last] = el;
-            ++last;
-        }
-        while(last < 5)
-        {
-            this->board[last] = Card();
-            ++last;
-        }
-        players.push_back(Player(host_hand));
-        players[0].process(this->board);
-        players[0].host = true;
-        for(int i(1); i < players_num; ++i)
-        {
-            players.push_back(Player(std::make_pair(Card(),Card())));
-            (*players.rbegin()).process(this->board);
-        }
-        std::sort(players.begin(),players.end(),std::greater<Player>());
-        win = players[0].host;
+        this->board[last] = el;
+        ++last;
     }
-};
+    while(last < 5)
+    {
+        this->board[last] = Card();
+        ++last;
+    }
+    players.push_back(Player(host_hand));
+    players[0].process(this->board);
+    players[0].host = true;
+    for(int i(1); i < players_num; ++i)
+    {
+        players.push_back(Player(std::make_pair(Card(),Card())));
+        (*players.rbegin()).process(this->board);
+    }
+    std::sort(players.begin(),players.end(),std::greater<Player>());
+    win = players[0].host;
+}
+
+std::ostream& operator<<(std::ostream& out, const Game& g)
+{
+    out << "Board: ";
+    for(Card c : g.board)
+    {
+        out << '|' << CardValuesOut[c.value] << CardSuitsOut[c.suit] << '|';
+    }
+    out << '\n';
+    for(Player p : g.players)
+    {
+        out << "-----------------" << '\n';
+        out << "Hand: " << '|' << CardValuesOut[p.hand.first.value] << CardSuitsOut[p.hand.first.suit] << '|' << CardValuesOut[p.hand.second.value] << CardSuitsOut[p.hand.second.suit] << '|';
+        out << '\n';
+        out << CombinationsOut[(*p.combinations.begin()).c_type];
+        if(*p.combinations.begin() == *(*g.players.begin()).combinations.begin())
+            out << '\n' << "Winner";
+        out << '\n';
+    }
+    out << "-----------------" << '\n';
+
+    return out;
+}
 
 int main()
 {
     Game* g = new Game(std::make_pair(Card(ace,heart),Card(jack,spade)),std::vector<Card>(),4);
+    std::cout << *g << '\n';
     return 0;
 }
